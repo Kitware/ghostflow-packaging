18ea005c867013a3a5e9f41391d0dee6fc90a746
Ryan Krattiger <ryan.krattiger@kitware.com>
2025-07-17 16:33:31 -0500

    Options for install and vendor

--- docker/builder/install.sh
+++ docker/builder/install.sh
@@ -1,6 +1,16 @@
 set -e
 set -x
 
+do_install=1
+while [ $# -gt 0 ]; do
+  case "$1" in
+    --install) do_install=1; shift;;
+    --vendor) do_install=0; shift;;
+    -*) echo "unrecognized option: $1"; shift;;
+    *) break;;
+  esac
+done
+
 app=$1
 ref=${2:-master}
 
@@ -22,14 +32,16 @@ cargo fetch --locked
 mkdir -p .cargo/
 cargo vendor > .cargo/config.toml
 
-# Install to /app/
-cargo install --frozen --path . --root /app/ || cargo install --frozen --path ${app} --root /app/
-
 # Create the tarball
-cd ../
-tar --exclude .gitlab/ \
-    --exclude .git/ \
-    --exclude target/ \
-    -cJ ./${app}-${ref} \
-    -f ${app}-${ref}-vendored.tar.xz
-chmod 666 ${app}-${ref}-vendored.tar.xz
+if [ "${do_install}" -eq "1" ]; then
+  # Install to /app/
+  cargo install --frozen --path . --root /app/ || cargo install --frozen --path ${app} --root /app/
+else
+  cd ../
+  tar --exclude .gitlab/ \
+      --exclude .git/ \
+      --exclude target/ \
+      -cJ ./${app}-${ref} \
+      -f ${app}-${ref}-vendored.tar.xz
+  chmod 666 ${app}-${ref}-vendored.tar.xz
+fi
